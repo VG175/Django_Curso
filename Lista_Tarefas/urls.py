0122048ce41eb594709833bdbf230673732dfa5a
Vitor <vg3396355@gmail.com>
2025-07-16 15:56:10 -0300

    versão -4
    Fazendo o ligamento do BD a parte de integração com o cliente.

--- Lista_Tarefas/urls.py
+++ Lista_Tarefas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.contrib import admin
+
 
 urlpatterns = [
-   #path('admin/', admin.site.urls),    
+    path('admin/', admin.site.urls),    
     path('',include('tarefas.urls')),
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from.models import Tarefa
 
 # Create your views here.
 def listaTarefa(request):
-    return render(request, 'tarefas/list.html')
+    Tarefas_list = Tarefa.objects.all().order_by('-created_at')
+    return render(request, 'tarefas/list.html',{'tarefas': Tarefas_list})
